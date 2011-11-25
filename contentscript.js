8c7090ddd15eed31ebb4f37ed4f5f004a9084f8f
Joseph Bass <jab416171@gmail.com>
2011-11-24 20:26:29 -0700

    successfully added a link to the message

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,37 @@
-onload = setTimeout(init,0);
+onload = setTimeout(init, 0);
 
 function init() {
-    alert(document.getElementById("145eb3db563c7117aa27e2f57bf517783f49b8fc"));
+    console.log("LOGGING STARTING");
+
+    var LENGTH_OF_MESSAGE_ID = 40;
+    var messageList = document.getElementById("gc-message-list");
+//    console.log(messageList);
+    var usefulMessages = new Array();
+    var messageListChildren = messageList.children;
+    var count = 0;
+    for (var i = 0; i < messageListChildren.length; i++) {
+        if (null !== messageListChildren[i].getAttribute("id") && LENGTH_OF_MESSAGE_ID == messageListChildren[i].getAttribute("id").length) { // it's an actual message
+            usefulMessages[count++] = messageListChildren[i];
+        }
+    }
+
+    console.log(usefulMessages.length);
+    for (var i = 0; i < usefulMessages.length; i++) {
+        var child = document.createElement('div');
+        var anchor = document.createElement('a');
+        anchor.setAttribute("href", "http://www.google.com");
+        anchor.innerText = "click me";
+        child.setAttribute("class", "gc-message-sms-row");
+        child.appendChild(anchor);
+        usefulMessages[i].children[0].children[1].children[0].children[0].children[0].children[2].children[0].children[0].children[0].
+                children[1].children[1].children[0].children[0].children[1].children[0].children[0].children[0].children[0].children[0].children[0].
+                children[1].children[0].children[0].children[0].appendChild(child);
+    }
+    console.log(usefulMessages);
+
+    console.log("LOGGING ENDING");
 }
+
+
+
+
