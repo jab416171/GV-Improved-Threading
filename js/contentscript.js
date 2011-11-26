1e3ee6d93f459c8295dc9977cf9136698f68e048
Joseph Bass <jab416171@gmail.com>
2011-11-26 14:34:16 -0700

    added newer and older links

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,40 +1,32 @@
 $(document).ready(function() {
-	onload = setTimeout(init, 0);
-	function init() {
-		console.log("LOGGING STARTING");
+//    onload = setTimeout(init, 0);
+    function init() {
+        var actionLinks = $('<div class="gc-message-sms-row"></div>');
+        var newer = $('<a class="newer" href="#">newer</a>');
+        $(actionLinks).append(newer);
+        var older = $('<a class="older" href="#">older</a>');
+        $(actionLinks).prepend(older);
+        var gcMessageDisplay = $('.gc-message-message-display');
+        console.log(gcMessageDisplay.append(actionLinks));
+        gcMessageDisplay.each(function(index, value) {
+            console.log("looping");
 
-		var LENGTH_OF_MESSAGE_ID = 40;
-		var messageList = document.getElementById("gc-message-list");
-	//    console.log(messageList);
-		var usefulMessages = new Array();
-		var messageListChildren = messageList.children;
-		var count = 0;
-		for (var i = 0; i < messageListChildren.length; i++) {
-			if (null !== messageListChildren[i].getAttribute("id") && LENGTH_OF_MESSAGE_ID == messageListChildren[i].getAttribute("id").length) { // it's an actual message
-				usefulMessages[count++] = messageListChildren[i];
-			}
-		}
+            $($('.newer').eq(index)).on('click', function(event) {
+                event.preventDefault();
+                var parent = $(this).parent();
+                console.log(gcMessageDisplay);
+                console.log(index);
+                parent.html(gcMessageDisplay.eq(index + 1));
+            });
+            $($('.older').eq(index)).on('click', function(event) {
+                event.preventDefault();
+                var parent = $(this).parent();
+                console.log(gcMessageDisplay);
+                console.log(index);
+                parent.html(gcMessageDisplay.eq(index - 1));
+            });
+        });
+    }
 
-		console.log(usefulMessages.length);
-		for (var i = 0; i < usefulMessages.length; i++) {
-			var actionLinks = $('<div class="gc-message-sms-row"></div>')
-			var next = $('<a class="next">next</a>');
-			$(actionLinks).append(next);
-			console.log($('.gc-message-message-display')[i].appendChild(actionLinks));
-			//usefulMessages[i].children[0].children[1].children[0].children[0].children[0].children[2].children[0].children[0].children[0].
-			//        children[1].children[1].children[0].children[0].children[1].children[0].children[0].children[0].children[0].children[0].children[0].
-			//        children[1].children[0].children[0].children[0].appendChild(child);
-		}
-		console.log(usefulMessages);
-
-		console.log("LOGGING ENDING");
-	}
-	
-	$('.clickMe').live('click', function() {
-		console.log($(this));
-	});
-	
+    init();
 });
-
-
-
